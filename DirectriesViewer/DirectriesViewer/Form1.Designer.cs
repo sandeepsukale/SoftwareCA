96ddd563ded9a593de480e22de3e766ec082c844
Maryabeeb <10519008@mydbs.ie>
2019-04-15 14:59:54 +0100

    firstcommit

---
+++ DirectriesViewer/DirectriesViewer/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DirectriesViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.displayText = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Sdir = new System.Windows.Forms.ListBox();
+            this.AllFile = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.Sfile = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "OpenID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click on the browse button to select directory";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "browse Dir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // displayText
+            // 
+            this.displayText.Location = new System.Drawing.Point(90, 25);
+            this.displayText.Name = "displayText";
+            this.displayText.Size = new System.Drawing.Size(169, 20);
+            this.displayText.TabIndex = 2;
+            this.displayText.Text = "Display Text from path here";
+            // 
+            // Sdir
+            // 
+            this.Sdir.FormattingEnabled = true;
+            this.Sdir.Location = new System.Drawing.Point(24, 92);
+            this.Sdir.Name = "Sdir";
+            this.Sdir.Size = new System.Drawing.Size(151, 277);
+            this.Sdir.TabIndex = 3;
+            // 
+            // AllFile
+            // 
+            this.AllFile.FormattingEnabled = true;
+            this.AllFile.Location = new System.Drawing.Point(354, 92);
+            this.AllFile.Name = "AllFile";
+            this.AllFile.Size = new System.Drawing.Size(245, 277);
+            this.AllFile.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sorted Directories";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sorted Files/Directory";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(413, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "All Sorted Files";
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Sfile
+            // 
+            this.Sfile.FormattingEnabled = true;
+            this.Sfile.Location = new System.Drawing.Point(192, 92);
+            this.Sfile.Name = "Sfile";
+            this.Sfile.Size = new System.Drawing.Size(135, 277);
+            this.Sfile.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 441);
+            this.Controls.Add(this.Sfile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AllFile);
+            this.Controls.Add(this.Sdir);
+            this.Controls.Add(this.displayText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FilesSorting App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+      
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox displayText;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox Sdir;
+        private System.Windows.Forms.ListBox AllFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.ListBox Sfile;
+    }
+}
+
---
+++ DirectriesViewer/DirectriesViewer/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DirectriesViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Multiselect = true;
+           openFileDialog1.RestoreDirectory = false;
+           
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+               
+
+
+                var filePath = folderBrowserDialog1.SelectedPath;
+               
+                    var files = Path.GetDirectoryName(filePath);
+
+                    string[] fileEntries = Directory.GetFiles(filePath);//list of all the file names in the directory
+                    displayText.Text = filePath;
+
+                    List<string> items = new List<string>();
+                    List<string> sortedfiles = new List<string>();
+                    List<string> Allsortedfiles = new List<string>();
+                    List<string> calcfile = new List<string>();
+                    items.Add(Path.GetFullPath(filePath));
+
+
+                    foreach (string fileName in fileEntries)//method to sort each file in file names
+                    {
+                        StreamReader str = new StreamReader(fileName);
+
+
+
+                        string FileContent = str.ReadToEnd();
+                        
+
+                    if (Path.GetExtension(fileName) == ".txt")
+                    {
+                        
+                         string[] sorted= FileContent.Split(' ');
+
+                        Array.Sort(sorted);
+
+                        string sortedFile = "\\Sorted" + Path.GetFileName(fileName);
+                        string path = filePath + sortedFile;
+
+                        if (!File.Exists(path))
+                        {
+
+                           
+                            StreamWriter sw = new StreamWriter(path);
+                            foreach (string item in sorted)
+                            {
+                                
+                                    sw.WriteLine(item);
+                                
+                               
+                            }
+
+
+                            sortedfiles.Add(sortedFile);
+                            sw.Close();
+
+                            Allsortedfiles.Add(sortedFile + " in " + Directory.GetDirectoryRoot(path));
+                        }
+                        else { MessageBox.Show("{0}  already eXists", Path.GetFileName(path)); }
+                    }
+
+                    
+                 
+
+                    Sdir.DataSource = items;
+                    Sfile.DataSource = sortedfiles;
+                    AllFile.DataSource = Allsortedfiles;
+
+           }
+             
+            }
+            
+        }
+      
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
